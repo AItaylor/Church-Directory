6793b6c4b181b9b9f0c98935b5e7ba3bc8221a3f
MICHAEL TAYLOR <michael.taylor271@outlook.com>
2024-08-02 12:34:54 -0500

    add members data.js

--- data.js
+++ data.js
@@ -7,5 +7,29 @@ const members = [
         role: "Deacon",
         family: "Doe Family"
     },
+    {
+        id: 2,
+        name: "Jane Doe",
+        photo: "https://example.com/john-doe.jpg",
+        contact: "john@example.com",
+        role: "Deacon",
+        family: "Doe Family"
+    },
+    {
+        id: 3,
+        name: "Karen Smith",
+        photo: "https://example.com/john-doe.jpg",
+        contact: "john@example.com",
+        role: "Deacon",
+        family: "Doe Family"
+    },
+    {
+        id: 4,
+        name: "Alex Koe",
+        photo: "https://example.com/john-doe.jpg",
+        contact: "john@example.com",
+        role: "Deacon",
+        family: "Doe Family"
+    },
     // Add more members...
 ];
